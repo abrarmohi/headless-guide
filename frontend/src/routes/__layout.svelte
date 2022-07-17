19b982fa983165c6cbd73a5c064a0e09128fa2a9
Abrar Mohi Shafee <abrarmohi@yahoo.com>
2022-07-17 04:09:57 +0200

    add tailwind

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import '../app.css'
+</script>
+
+<div class="container p-3 flex flex-col gap-3">
+	<header class="header flex flex-col gap-3">
+		<div class="text-3xl">SSS</div>
+		<nav class="navigation">
+			<a href="/">Home</a>
+			<a href="/blog">About</a>
+		</nav>
+	</header>
+
+	<main class="main">
+		<slot />
+	</main>
+
+	<footer class="footer">
+		<p class="copyright">&copy; 2019</p>
+	</footer>
+</div>
