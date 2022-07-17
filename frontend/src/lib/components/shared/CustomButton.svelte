7addebe3ea2b1da81c99755eb452adcbd83b8382
Abrar Mohi Shafee <abrarmohi@yahoo.com>
2022-07-17 20:02:08 +0200

    add a custom 404 page

---
+++ frontend/src/lib/components/shared/CustomButton.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let link: string
+	export let newTab: boolean = false
+</script>
+
+<a href={link} target={newTab ? '_blank' : '_self'} class="no-hover-effect">
+	<button
+		class="custom-button font-bold py-2 px-4 rounded bg-royal-blue text-white hover:bg-blue-gem"
+	>
+		<slot />
+	</button>
+</a>
---
+++ frontend/src/routes/__error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	// Components
+	import CustomButton from '@components/shared/CustomButton.svelte'
+</script>
+
+<h1 class="text-5xl font-bold mb-5">Oops.</h1>
+<p class="text-xl mb-8">It's a dead end.</p>
+<CustomButton link="/">Run</CustomButton>
+
+<img class="m-auto" src="/404.webp" alt="404" />
